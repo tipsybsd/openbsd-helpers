3a8d357def267cc846a69f8ac4f0a74f1ec79222
tipsybsd <alexander.4mail@gmail.com>
2021-10-29 13:19:13 +0300

    mon_on now can work with external DP-1

--- sh-helpers/mon_on.sh
+++ sh-helpers/mon_on.sh
@@ -1,16 +1,44 @@
 xrandr > /dev/null
 
+# check for internal display interface
+if [ "`xrandr | grep LVDS-1  | cut -d ' ' -f 2`" = 'connected' ]
+then
+  INTERNAL_DISPLAY=LVDS-1
+elif [ "`xrandr | grep eDP-1  | cut -d ' ' -f 2`" = 'connected' ]
+then
+  INTERNAL_DISPLAY=eDP-1
+fi
+
+# we suggest, what if we have VGA-1 connected, when we have two external displays case
 if [ "`xrandr | grep VGA-1  | cut -d ' ' -f 2`" = 'connected' ]
 then
-    xrandr --output eDP-1 --off    
-    xrandr --output VGA-1 --auto
-    #xrandr --output HDMI-1 --mode 1920x1200
+  xrandr --output ${INTERNAL_DISPLAY} --off    
+  xrandr --output VGA-1 --auto
+
+  if  [ "`xrandr | grep HDMI-1  | cut -d ' ' -f 2`" = 'connected' ]
+  then
     xrandr --output HDMI-1 --auto
     xrandr --output HDMI-1 --right-of VGA-1
+  fi
+
+  if  [ "`xrandr | grep DP-1  | cut -d ' ' -f 2`" = 'connected' ]
+  then
+    xrandr --output DP-1 --auto
+    xrandr --output DP-1 --right-of VGA-1
+  fi
 else
-    #xrandr --output HDMI-1 --mode 1920x1200
-    xrandr --output HDMI-1 --auto
-    xrandr --output HDMI-1 --right-of eDP-1
+
+# ... else - just activate second display
+ if [ "`xrandr | grep HDMI-1  | cut -d ' ' -f 2`" = 'connected' ]
+ then
+   xrandr --output HDMI-1 --auto
+   xrandr --output HDMI-1 --right-of ${INTERNAL_DISPLAY}
+ elif [ "`xrandr | grep DP-1  | cut -d ' ' -f 2`" = 'connected' ]
+ then 
+   xrandr --output DP-1 --auto
+   xrandr --output DP-1 --right-of ${INTERNAL_DISPLAY}
+ fi
+
 fi    
 
 feh --bg-scale ~/.wallpaper &
