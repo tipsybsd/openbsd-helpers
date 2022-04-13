efe75cda8b2199083aed03becfdc992ad69226bd
tipsybsd <alexander.4mail@gmail.com>
2022-04-14 01:03:48 +0300

    moved chromium to tag 5, fix firefox tag

--- conf/dwm/config.def.h
+++ conf/dwm/config.def.h
@@ -32,12 +32,12 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 2,       0,           -1 },
-	{ "Chromium", NULL,       NULL,       1 << 2,       0,           -1 },
-	{ "Xonix",    NULL,       NULL,       0,            1,           -1 },
-	{ NULL,       NULL,       "glxgears", 0,            1,           -1 },
+	/* class       instance    title       tags mask     isfloating   monitor */
+	{ "Gimp",      NULL,       NULL,       0,            1,           -1 },
+	{ "firefox-default", NULL,       NULL,       1 << 2,       0,           -1 },
+	{ "Chromium",  NULL,       NULL,       1 << 4,       0,           -1 },
+	{ "Xonix",     NULL,       NULL,       0,            1,           -1 },
+	{ NULL,        NULL,       "glxgears", 0,            1,           -1 },
 };
 
 /* layout(s) */
